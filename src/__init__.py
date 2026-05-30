66e1fa336efd71f46693c85aee6e0f8a0395bedf
zx0518zx <1095772031@qq.com>
2026-05-30 19:02:43 +0800

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 from .ies_parser import parse_ies_full
-from .config import register_species, register_light, get_ppfd_factor, get_available_species
+# 👇 在这里补上 get_species_params 和 get_available_lights
+from .config import register_species, register_light, get_ppfd_factor, get_available_species, get_species_params, get_available_lights
 
 # 导入整合后的模块
 from .light_analysis import calculate_canopy_ppfd, grade_light_environment, visualize_ppfd_3d
@@ -14,5 +15,7 @@ __all__ = [
     "register_species",
     "register_light",
     "get_ppfd_factor",
-    "get_available_species"
+    "get_available_species",
+    "get_species_params",     
+    "get_available_lights"    
 ]
